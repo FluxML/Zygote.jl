1d2476623b1b686a81d42c1542e4147a3ed40f1c
Mike Innes <mike.j.innes@gmail.com>
2018-04-10 02:57:13 +0100

    just reverse control flow

--- src/reverse.jl
+++ src/reverse.jl
@@ -1,31 +1,41 @@
+struct Forward
+  id::Int
+end
+
+Base.show(io::IO, x::Forward) = print(io, "@", x.id)
+
 # Only the final BB can return (so we have a single entry point in reverse).
 # TODO: merge return nodes
 validcfg(ir) =
   isexpr(ir.stmts[end], :return) &&
   !any(x -> isexpr(x, :return), ir.stmts[1:end-1])
 
-function recbranches!(ir::IRCode)
+# Insert Phi nodes which record branches taken
+function record_branches!(ir::IRCode)
   for b in blocks(ir)
     ps = BasicBlock(b).preds
     length(ps) > 1 || continue
     @assert length(ps) == 2
-    insert!(b, 1, Phi(ps, [true, false]))
+    insert!(b, 1, Phi(ps, [false, true]))
   end
   return ir
 end
 
-reverse(x) = Base.reverse(x)
-
-function reverse(ir::IRCode)
+function reverse_blocks(ir::IRCode)
+  @assert validcfg(ir)
   rev = IRCode([], CFG([], []))
-  bs = revblocks(ir.cfg)
   newidx(i) = length(ir.cfg.blocks)-i+1
   for b in reverse(blocks(ir))
-    newblock!(rev, succs = newidx.(BasicBlock(b).preds), preds = newidx.(BasicBlock(b).succs))
-    for i in reverse(range(b))
-      ex = ir.stmts[i]
-      (isexpr(ex, :gotoifnot) || ex isa GotoNode) && continue
-      push!(rev, ir.stmts[i])
+    succs, preds = newidx.(BasicBlock(b).preds), newidx.(BasicBlock(b).succs)
+    newblock!(rev, succs = succs, preds = preds)
+    @assert length(succs) ≤ 2
+    if length(succs) == 1
+      push!(rev, GotoNode(succs[1]))
+    elseif length(succs) == 2
+      push!(rev, Expr(:gotoifnot, Forward(range(b)[1]), succs[1]))
+      push!(rev, GotoNode(succs[2]))
+    else
+      push!(rev, nothing)
     end
   end
   return rev
