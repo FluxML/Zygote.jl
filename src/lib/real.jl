72d7bbb2eb6c016a6d7d04b753251789e2d51946
Mike J Innes <mike.j.innes@gmail.com>
2018-05-16 14:44:19 +0700

    use diffrules

--- src/lib/real.jl
+++ src/lib/real.jl
@@ -1,9 +1,21 @@
+using DiffRules, SpecialFunctions, NaNMath
+
 grad(x::Real) = zero(x)
 grad(x::Integer) = zero(float(x))
 
-# TODO: DiffRules
-@grad sin(x) = sin(x), Δ -> (cos(x)*Δ,)
-@grad cos(x) = cos(x), Δ -> (-sin(x)*Δ,)
+for (M, f, arity) in DiffRules.diffrules()
+  arity == 1 || continue
+  @eval begin
+    @grad $M.$f(x) = $M.$f(x),
+      Δ -> (Δ * $(DiffRules.diffrule(M, f, :x)),)
+  end
+end
 
-@grad a + b = a+b, Δ -> (Δ, Δ)
-@grad a * b = a*b, Δ -> (Δ*b', a'*Δ)
+for (M, f, arity) in DiffRules.diffrules()
+  arity == 2 || continue
+  da, db = DiffRules.diffrule(M, f, :a, :b)
+  @eval begin
+    @grad $M.$f(a, b) = $M.$f(a, b),
+      Δ -> (Δ * $da, Δ * $db)
+  end
+end
