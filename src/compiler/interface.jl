b14c0499086fd703ba80090e2892527aa519d8f6
Mike J Innes <mike.j.innes@gmail.com>
2018-05-18 00:17:35 +0700

    handle varargs functions

--- src/compiler/interface.jl
+++ src/compiler/interface.jl
@@ -4,14 +4,18 @@ end
 
 isprimitive(f) = f isa Core.Builtin || f isa Core.IntrinsicFunction
 
+function unsplat(f, args...)
+  m = which(f, typesof(args...))
+  m.isva || return args
+  (args[1:m.nargs-2]...,args[m.nargs-1:end])
+end
+
 function _forward(f, args...)
   isprimitive(f) && return (f(args...), Δ -> map(_ -> nothing, args))
   ir = code_ir(f, typesof(args...))
   forw, back = stacks!(grad_ir(ir))
-  y, J = interpret(forw, f, args...)
-  return y, function (Δ)
-    interpret(back, J, Δ)
-  end
+  y, J = interpret(forw, f, unsplat(f, args...)...)
+  return y, Δ -> interpret(back, J, Δ)
 end
 
 function forward(f, args...)
