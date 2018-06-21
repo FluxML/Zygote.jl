72922a5ec78b9ddebf0829e6e14e4b08ea45f69a
Mike Innes <mike.j.innes@gmail.com>
2018-06-21 18:14:41 +0100

    optional compilation

--- src/Zygote.jl
+++ src/Zygote.jl
@@ -15,4 +15,10 @@ include("lib/lib.jl")
 include("lib/real.jl")
 include("lib/array.jl")
 
+# 1. we need to define this late, so that the genfuncs see lib.jl
+# 2. we make it optional until more tests pass in compiled mode
+function compiled()
+  include(joinpath(@__DIR__, "compiler/interface2.jl"))
+end
+
 end # module
--- src/compiler/interface.jl
+++ src/compiler/interface.jl
@@ -16,26 +16,7 @@ J{S}(x) where S = J{S,typeof(x)}(x)
 
 Base.show(io::IO, j::J{S}) where S = print(io, "J{$(S.parameters[1])}(...)")
 
-@generated function _forward(ctx::Context, f, args...)
-  T = Tuple{f,args...}
-  (g = _lookup_grad(T)) == nothing && return :(f(args...), J{$T}(()))
-  meta, forw, _ = g
-  forw = varargs!(meta, forw, 3)
-  update!(meta, forw)
-  meta.code.slotnames = [Symbol("#self#"), :ctx, :f, :args]
-  return meta.code
-end
-
-@generated function (j::J{T})(Δ) where T
-  meta, _, back = _lookup_grad(T)
-  meta.code.slottypes = Any[j, Δ]
-  meta.code.slotnames = Any[Symbol("#self#"), :Δ]
-  update!(meta, back)
-  # Enable type inference
-  meta.code.inferred = false
-  meta.code.ssavaluetypes = length(meta.code.ssavaluetypes)
-  return meta.code
-end
+# interface2.jl
 
 # Interpreted mode
 
