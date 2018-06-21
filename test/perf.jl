895c61295f694fd0d5064d84c9b3d50f5f9bc53b
Mike J Innes <mike.j.innes@gmail.com>
2018-06-22 00:59:56 +0100

    basic compilation tests

--- test/perf.jl
+++ test/perf.jl
@@ -1,11 +1,19 @@
 using Zygote, Test
 using Zygote: forward
 
+Zygote.compiled()
+
 @testset "Performance" begin
 
 y, back = @inferred forward(*, 2, 3)
 @test y == 6
 
-@test @inferred(back(1)) == (3 ,2)
+@test @inferred(back(1)) == (3, 2)
+
+_sincos(x) = sin(cos(x))
+
+y, back = @inferred forward(_sincos, 0.5)
+@test y == _sincos(0.5)
+@inferred back(1)
 
 end
