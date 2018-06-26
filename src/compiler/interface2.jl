fee8bd046e62072af2e31558fe0950d4db86d82d
Mike J Innes <mike.j.innes@gmail.com>
2018-06-26 09:24:44 +0100

    error messages

--- src/compiler/interface2.jl
+++ src/compiler/interface2.jl
@@ -1,6 +1,6 @@
 @generated function _forward(ctx::Context, f, args...)
   T = Tuple{f,args...}
-  (g = _lookup_grad(T)) == nothing && return :(f(args...), J{$T}(()))
+  (g = _lookup_grad(T)) == nothing && return :(f(args...), J{$T}((f,)))
   meta, forw, _ = g
   forw = varargs!(meta, forw, 3)
   forw = inlineable!(forw)
@@ -10,6 +10,7 @@
 end
 
 @generated function (j::J{T})(Δ) where T
+  (g = _lookup_grad(T)) == nothing && return :(error("Non-differentiable function $(j.t[1])"))
   meta, _, back = _lookup_grad(T)
   resize!(back.argtypes, 2)
   meta.code.slottypes = Any[j, Δ]
