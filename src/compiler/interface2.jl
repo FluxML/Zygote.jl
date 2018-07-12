4c7147a6f3fce508a03b981d1bfa1e91275af121
Mike J Innes <mike.j.innes@gmail.com>
2018-07-12 14:41:14 +0100

    throw compile errors in backward pass

--- src/compiler/interface2.jl
+++ src/compiler/interface2.jl
@@ -1,6 +1,7 @@
 @generated function _forward(ctx::Context, f, args...)
   T = Tuple{f,args...}
-  (g = _lookup_grad(T)) == nothing && return :(f(args...), J{$T}((f,)))
+  g = try _lookup_grad(T) catch e e end
+  !(g isa Tuple) && return :(f(args...), J{$T}((f,)))
   meta, forw, _ = g
   forw = varargs!(meta, forw, 3)
   forw = inlineable!(forw)
@@ -10,8 +11,9 @@
 end
 
 @generated function (j::J{T})(Δ) where T
-  (g = _lookup_grad(T)) == nothing && return :(error("Non-differentiable function $(j.t[1])"))
-  meta, _, back = _lookup_grad(T)
+  g = _lookup_grad(T)
+  g == nothing && return :(error("Non-differentiable function $(j.t[1])"))
+  meta, _, back = g
   resize!(back.argtypes, 2)
   meta.code.slottypes = Any[j, Δ]
   meta.code.slotnames = Any[Symbol("#self#"), :Δ]
