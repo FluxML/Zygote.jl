f12026818bd78191b341d738089bf13daf77e840
Mike J Innes <mike.j.innes@gmail.com>
2018-08-22 18:22:25 +0100

    fix inference tests

--- test/compiler.jl
+++ test/compiler.jl
@@ -2,7 +2,7 @@ using Zygote, Test
 using Zygote: forward
 
 macro test_inferred(ex)
-  :(let res
+  :(let res = nothing
     @test begin
       res = @inferred $ex
       true
@@ -19,9 +19,12 @@ _sincos(x) = sin(cos(x))
 y, back = @test_inferred forward(_sincos, 0.5)
 @test_inferred back(1)
 
-y, back = @test_inferred forward(pow, 2, 3)
-@test_inferred back(1)
+dpow(n, p) = something(Zygote.gradient(pow, n, p)[1], zero(n))
+
+@test_inferred forward(pow, 2, 3)
+@test_inferred dpow(2, 3)
 
 cube(x) = pow(x, 3)
+dcube(x) = something(Zygote.derivative(cube, x), zero(x))
 y, back = @test_inferred forward(cube, 2)
-@test_inferred back(1)
+@test_inferred dcube(2)
