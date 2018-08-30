022c681a05dd1bf8a8b2736707f20285a4436c24
Mike J Innes <mike.j.innes@gmail.com>
2018-08-30 10:41:55 +0100

    unwrap flux tracking

--- src/flux.jl
+++ src/flux.jl
@@ -1,4 +1,5 @@
 using .Flux.Tracker: TrackedArray, TrackedReal
 
-# TODO: enable this once we can work on typed IR
-# unwrap(x::Union{TrackedArray,TrackedReal}) = Flux.data(x)
+if !usetyped
+  unwrap(x::Union{TrackedArray,TrackedReal}) = Flux.data(x)
+end
