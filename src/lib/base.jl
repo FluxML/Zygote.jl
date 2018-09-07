81474da81e5c282dd6684fe71d0de07cbbf3ff3b
Mike J Innes <mike.j.innes@gmail.com>
2018-09-07 15:11:13 +0100

    dict fix

--- src/lib/base.jl
+++ src/lib/base.jl
@@ -4,11 +4,17 @@ using Base: @get!
 
 grad_mut(d::AbstractDict) = Dict()
 
+# TODO perhaps look up mutable gradients in `forward`
+function accum(a::AbstractDict, b::AbstractDict)
+  @assert a === b
+  return a
+end
+
 @grad function getindex(d::AbstractDict, k)
   d[k], function (Δ)
     grad = grad_mut(__context__, d)
     grad[k] = accum(get(grad, k, nothing), Δ)
-    return
+    return (grad, nothing)
   end
 end
 
