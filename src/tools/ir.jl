34d1798e6dbff22fce2c195c7278e580a50b6c39
Mike J Innes <mike.j.innes@gmail.com>
2018-09-11 12:54:15 +0100

    use reflection from irtools

--- src/tools/ir.jl
+++ src/tools/ir.jl
@@ -6,23 +6,6 @@ import Core.Compiler: CodeInfo, IRCode, CFG, BasicBlock, Argument, ReturnNode,
   compact!, finish, DomTree, construct_domtree, dominates, userefs, widenconst, types, verify_ir
 using InteractiveUtils: typesof
 
-for T in :[IRCode, IncrementalCompact, Compiler.UseRef, Compiler.UseRefIterator, Compiler.TypesView].args
-  @eval begin
-    Base.getindex(ir::$T, a...) = Compiler.getindex(ir, a...)
-    Base.setindex!(ir::$T, a...) = Compiler.setindex!(ir, a...)
-  end
-end
-
-Base.getindex(r::StmtRange, i) = (r.first:r.last)[i]
-
-for T in :[UseRefIterator, IncrementalCompact, Pair].args
-  @eval Base.iterate(x::Compiler.$T, a...) = Compiler.iterate(x, a...)
-end
-
-PhiNode(x, y) = PhiNode(Any[x...], Any[y...])
-
-CFG(bs) = CFG(bs, map(b -> b.stmts.first, bs[2:end]))
-
 afterphi(ir, loc) = ir.stmts[loc] isa PhiNode ? afterphi(ir, loc+1) : loc
 
 insert_blockstart!(ir::IRCode, pos, typ, val) =
@@ -114,49 +97,4 @@ function domfront(cfg, dt = construct_domtree(cfg))
     end
   end
   return fronts
-end
-
-# SSA contruction (forked from Base for untyped code)
-
-import Core.Compiler: normalize, strip_trailing_junk!, compute_basic_blocks,
-  scan_slot_def_use, LineInfoNode, construct_ssa!, IR_FLAG_INBOUNDS
-
-function just_construct_ssa(ci::CodeInfo, code::Vector{Any}, nargs::Int, sp)
-  ci.ssavaluetypes = Any[Any for _ = 1:length(code)]
-  slottypes = Any[Any for _ = 1:length(ci.slotnames)]
-  inbounds_depth = 0 # Number of stacked inbounds
-  meta = Any[]
-  flags = fill(0x00, length(code))
-  for i = 1:length(code)
-    stmt = code[i]
-    if isexpr(stmt, :inbounds)
-      arg1 = stmt.args[1]
-      if arg1 === true # push
-        inbounds_depth += 1
-      elseif arg1 === false # clear
-        inbounds_depth = 0
-      elseif inbounds_depth > 0 # pop
-        inbounds_depth -= 1
-      end
-      stmt = nothing
-    else
-      stmt = normalize(stmt, meta)
-    end
-    code[i] = stmt
-    if !(stmt === nothing)
-      if inbounds_depth > 0
-        flags[i] |= IR_FLAG_INBOUNDS
-      end
-    end
-  end
-  strip_trailing_junk!(ci, code, flags)
-  cfg = compute_basic_blocks(code)
-  defuse_insts = scan_slot_def_use(nargs, ci, code)
-  domtree = construct_domtree(cfg)
-  ir = let code = Any[nothing for _ = 1:length(code)]
-    argtypes = slottypes[1:(nargs+1)]
-    IRCode(code, Any[], ci.codelocs, flags, cfg, collect(LineInfoNode, ci.linetable), argtypes, meta, sp)
-  end
-  ir = construct_ssa!(ci, code, ir, domtree, defuse_insts, nargs, sp, slottypes)
-  return ir
 end
