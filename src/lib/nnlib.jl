c37f43ebf1a11ee08920ae8376a8e7eaae867fed
Keno Fischer <keno@juliacomputing.com>
2018-10-16 18:29:54 -0400

    Switch to tparam based NNlib interface

--- src/lib/nnlib.jl
+++ src/lib/nnlib.jl
@@ -1,15 +1,15 @@
 using NNlib
-import NNlib: softmax, ∇softmax, logsoftmax, ∇logsoftmax, conv, maxpool, meanpool
+import NNlib: softmax, ∇softmax, logsoftmax, ∇logsoftmax, conv, maxpool, meanpool, _conv
 
 @adjoint softmax(xs) = softmax(xs), Δ -> (∇softmax(Δ, xs),)
 
 @adjoint logsoftmax(xs) = logsoftmax(xs), Δ -> (∇logsoftmax(Δ, xs),)
 
-@adjoint conv(x, w; kw...) =
-  conv(x, w; kw...),
+@adjoint (::_conv{pad, stride, dilation})(x, w) where {pad, stride, dilation} =
+  _conv{pad, stride, dilation}()(x, w),
     Δ ->
-      (NNlib.∇conv_data(Δ, x, w; kw...),
-       NNlib.∇conv_filter(Δ, x, w; kw...))
+      (NNlib._∇conv_data{pad, stride, dilation, 0}()(Δ, x, w),
+       NNlib._∇conv_filter{pad, stride, dilation, 0}()(Δ, x, w))
 
 @adjoint function maxpool(x, k; kw...)
   y = maxpool(x, k; kw...)
