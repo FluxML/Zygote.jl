5880badc76ba292b678537088691503825ce760d
Mike J Innes <mike.j.innes@gmail.com>
2018-10-31 18:24:03 +0000

    complex log fixup

--- src/lib/complex.jl
+++ src/lib/complex.jl
@@ -1,4 +1,4 @@
-@grad real(x::Complex) = (real(x), r̄ -> (r̄ + zero(r̄)*im,))
-@grad imag(x::Complex) = (imag(x), ī -> (zero(ī) + ī*im,))
+@grad real(x::Complex) = real(x), r̄ -> (r̄ + zero(r̄)*im,)
+@grad imag(x::Complex) = imag(x), ī -> (zero(ī) + ī*im,)
 
-@grad log(x::Complex) = (log(x), ȳ -> (1/ȳ,))
+@grad log(x::Complex) = log(x), ȳ -> (ȳ/x,)
