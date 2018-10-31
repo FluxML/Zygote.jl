3b45385dca233d68e76217f99cce66faffd76857
Mike J Innes <mike.j.innes@gmail.com>
2018-10-31 09:55:49 +0000

    reverse mode bcast

--- src/lib/broadcast.jl
+++ src/lib/broadcast.jl
@@ -15,7 +15,33 @@
 using Base.Broadcast
 using Base.Broadcast: Broadcasted, AbstractArrayStyle, DefaultArrayStyle, broadcasted,
   instantiate, materialize, flatten, combine_eltypes, _broadcast_getindex
-using ForwardDiff: Dual
+
+# Structural utilities
+
+using Base: tail
+
+tcat(x) = x
+tcat(x, y, z...) = tcat((x..., y...), z...)
+
+broadcast_args(x) = (x,)
+broadcast_args(bc::Broadcasted) = tcat(map(broadcast_args, bc.args)...)
+
+_unflatten(x, xs) = first(xs), tail(xs)
+
+_unflatten(x::Tuple{}, xs) = (), xs
+
+function _unflatten(x::Tuple, xs)
+  t1, xs1 = _unflatten(first(x), xs)
+  t2, xs2 = _unflatten(tail(x), xs1)
+  (t1, t2...), xs2
+end
+
+function _unflatten(bc::Broadcasted, xs)
+  t, xs′ = _unflatten(bc.args, xs)
+  (args=t,f=nothing,axes=nothing), xs′
+end
+
+unflatten(x, xs) = _unflatten(x, xs)[1]
 
 trim(x, Δ) = reshape(Δ, ntuple(i -> size(Δ, i), Val(ndims(x))))
 
@@ -25,6 +51,50 @@ unbroadcast(x::AbstractArray, Δ) =
     trim(x, sum(Δ, dims = ntuple(i -> size(x, i) == 1 ? i : ndims(Δ)+1, Val(ndims(Δ)))))
 
 unbroadcast(x::Number, Δ) = sum(Δ)
+
+# Reverse Mode
+
+Jtrivial(f, a...) = nothing
+Jtrivial(::typeof(+), a...) = a
+Jtrivial(::typeof(-), a, b) = (a..., .-b...)
+
+trivia(_) = (1,)
+function trivia(bc::Broadcasted)
+  t = trivia.(bc.args)
+  any(t -> t === nothing, t) && return
+  Jtrivial(bc.f, t...)
+end
+
+Joutput(f, a...) = nothing
+Joutput(::typeof(exp), x) = map(t -> y -> y*t, x)
+
+function Jbroadcast(bc::Broadcasted)
+  t = trivia.(bc.args)
+  any(t -> t === nothing, t) && return
+  Joutput(bc.f, t...)
+end
+
+@inline function unbroadcast_r(x, y, ȳ, j::J) where J
+  trim(x, j.(y).*ȳ)
+end
+
+@inline function unbroadcast_r(x::Number, y, ȳ, j::J) where J
+  x̄ = zero(float(x))
+  @simd for I in eachindex(y)
+    @inbounds x̄ += j(y[I])*ȳ[I]
+  end
+  return x̄
+end
+
+function ∇broadcast_r(bc::Broadcasted, J)
+  y = copy(instantiate(bc))
+  back(ȳ) = map(unbroadcast_r, broadcast_args(bc), map(_ -> y, J), map(_ -> ȳ, J), J)
+  return y, back
+end
+
+# Mixed Mode
+
+using ForwardDiff: Dual
 
 dual(x, p) = x
 dual(x::Real, p) = Dual(x, p)
@@ -71,25 +141,6 @@ function ∇broadcast(bc::Broadcasted{<:AbstractArrayStyle{0}})
   out = dualify(instantiate(flatten(bc)))[]
   return out.value, Δ -> map(x -> x*Δ, out.partials.values)
 end
-
-using Base: tail
-
-_unflatten(x, xs) = first(xs), tail(xs)
-
-_unflatten(x::Tuple{}, xs) = (), xs
-
-function _unflatten(x::Tuple, xs)
-  t1, xs1 = _unflatten(first(x), xs)
-  t2, xs2 = _unflatten(tail(x), xs1)
-  (t1, t2...), xs2
-end
-
-function _unflatten(bc::Broadcasted, xs)
-  t, xs′ = _unflatten(bc.args, xs)
-  (args=t,f=nothing,axes=nothing), xs′
-end
-
-unflatten(x, xs) = _unflatten(x, xs)[1]
 
 @grad function broadcasted(f, args...)
   broadcasted(f, args...), Δ -> (nothing, Δ.args...)
