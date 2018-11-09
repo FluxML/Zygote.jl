e3cd2fee6643dbcda8896aae330498ef036483be
Keno Fischer <keno@juliacomputing.com>
2018-11-08 19:55:13 -0500

    Fix inference for the meanpool gradient

--- src/lib/nnlib.jl
+++ src/lib/nnlib.jl
@@ -16,7 +16,15 @@ import NNlib: softmax, ∇softmax, logsoftmax, ∇logsoftmax, conv, maxpool, mea
   y, Δ -> (NNlib.∇maxpool(Δ, y, x, k; kw...), nothing)
 end
 
-@adjoint function meanpool(x, k; kw...)
-  y = meanpool(x, k; kw...)
-  y, Δ -> (NNlib.∇meanpool(Δ, y, x, k; kw...), nothing)
+struct ∇meanpool{k, pad, stride, T, S}
+  x::T
+  y::S
+end
+∇meanpool{k, pad, stride}(x::T, y::S) where {k, pad, stride, T, S} = ∇meanpool{k, pad, stride, T, S}(x, y)
+(mp::∇meanpool{k, pad, stride})(Δ) where {k, pad, stride} = (NNlib.∇meanpool(Δ, mp.y, mp.x, k; pad=pad, stride=stride), nothing)
+
+@adjoint function meanpool(x, k; pad = map(_->0,k), stride = k)
+  let y = meanpool(x, k; pad=pad, stride=stride)
+    y, ∇meanpool{k, pad, stride}(x, y)
+  end
 end
