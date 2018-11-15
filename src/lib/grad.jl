08da884e49b4d9d7036bf12a2a4d756de72a1909
Mike J Innes <mike.j.innes@gmail.com>
2018-11-15 03:37:58 +0000

    improve grad macro

--- src/lib/grad.jl
+++ src/lib/grad.jl
@@ -1,73 +1,50 @@
+using MacroTools
 using MacroTools: combinedef
+
+named(arg) = isexpr(arg, :(::)) && length(arg.args) == 1 ? :($(gensym())::$(arg.args[1])) : arg
+
+typeless(x) = MacroTools.prewalk(x -> isexpr(x, :(::)) ? x.args[1] : x, x)
 
 _gradtuple(::Nothing) = nothing
 _gradtuple(x::Tuple) = (nothing, x...)
 _gradtuple(x) = error("Gradient $x should be a tuple")
 
-function gradm(f, T, args, Ts, body, mut)
-  args = esc.(args)
-  Ts = esc.(Ts)
-  pushfirst!(args, :($(esc(:__context__))::Context), :($f::$T))
-  body = quote
-    Base.@_inline_meta
-    y, back = let
-      $(esc(body))
-    end
-    $(mut ? nothing : :(back2(::Nothing) = nothing))
-    # return needed for type inference
-    back2(Δ) = return _gradtuple(back(Δ))
-    y, back2
-  end
-  :(Zygote._forward($(args...)) where $(Ts...) = $body; nothing)
-end
-
 _gradtuple_kw(::Nothing) = nothing
 _gradtuple_kw(x::Tuple) = (nothing, nothing, nothing, x...) # kwfunc, kws, func, args...
 _gradtuple_kw(x) = error("Gradient $x should be a tuple")
-_untuple_kw(::Nothing) = nothing
-_untuple_kw(x::Tuple) = Base.tail(Base.tail(x))
 
-function gradm_kw(f, T, args, Ts, body, mut)
-  kws = popfirst!(args)
-  Ts = esc.(Ts)
-  kT = :(Core.kwftype($T))
-  kwargs = [:($(esc(:__context__))::Context), :(::$kT), :kw, f, esc.(args)...]
-  kw_wrappers = map(kws.args) do kw
-    kw isa Symbol && return :($(esc(kw)) = kw.$kw)
-    isexpr(kw, :...) && return :($(esc(kw.args[1])) = kw)
-    k, v = kw.args
-    :($(esc(k)) = haskey(kw, $(Expr(:quote, k))) ? kw.$k : $(esc(v)))
-  end
-  body = quote
-    Base.@_inline_meta
-    $(kw_wrappers...)
-    y, back = let
-      $(esc(body))
-    end
-    $(mut ? nothing : :(back2(::Nothing) = nothing))
-    # return needed for type inference
-    back2(Δ) = return _gradtuple_kw(back(Δ))
-    y, back2
-  end
-  quote
-    Zygote._forward($(kwargs...)) where $(Ts...) = $body
-    function Zygote._forward(cx::Context, f::$T, $(esc.(args)...)) where $(Ts...)
-      y, back = _forward(cx, Core.kwfunc(f), NamedTuple(), f, $(esc.(args)...)) # TODO unnamed arguments
-      return y, Δ -> _untuple_kw(back(Δ))
-    end
-    nothing
-  end
-end
+function grad end
 
 function gradm(ex, mut = false)
   @capture(shortdef(ex), (name_(args__) = body_) |
                          (name_(args__) where {Ts__} = body_)) || error("Need a function definition")
-  iskw = length(args) > 1 && isexpr(args[1], :parameters)
-  name, T = isexpr(name, :(::)) ?
-    (length(name.args) == 1 ? (:_, esc(name.args[1])) : esc.(name.args)) :
-    (:_, :(typeof($(esc(name)))))
+  kw = length(args) > 1 && isexpr(args[1], :parameters) ? esc(popfirst!(args)) : nothing
+  f, T = isexpr(name, :(::)) ?
+    (length(name.args) == 1 ? (esc(gensym()), esc(name.args[1])) : esc.(name.args)) :
+    (esc(gensym()), :(typeof($(esc(name)))))
+  kT = :(Core.kwftype($T))
   Ts == nothing && (Ts = [])
-  return (iskw ? gradm_kw : gradm)(name, T, args, Ts, body, mut)
+  args = esc.(named.(args))
+  argnames = typeless.(args)
+  Ts = esc.(Ts)
+  cx = :($(esc(:__context__))::Context)
+  fargs = kw == nothing ? [cx, :($f::$T), args...] : [kw, cx, :($f::$T), args...]
+  quote
+    @inline Zygote.grad($(fargs...)) where $(Ts...) = $(esc(body))
+    @inline function Zygote._forward($cx, $f::$T, $(args...)) where $(Ts...)
+      y, _back = grad(__context__, $f, $(argnames...))
+      $(mut ? nothing : :(back(::Nothing) = nothing))
+      back(Δ) = _gradtuple(_back(Δ))
+      return y, back
+    end
+    @inline function Zygote._forward($cx, ::$kT, kw, $f::$T, $(args...)) where $(Ts...)
+      y, _back = grad(__context__, $f, $(argnames...); kw...)
+      $(mut ? nothing : :(back(::Nothing) = nothing))
+      back(Δ) = _gradtuple_kw(_back(Δ))
+      return y, back
+    end
+    nothing
+  end
 end
 
 macro grad(ex)
