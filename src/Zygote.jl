d205223f8179fa9f208f8e97e3decb68819f5968
Will Tebbutt <wt0881@my.bristol.ac.uk>
2019-01-15 17:13:07 +0000

    Delay loading

--- src/Zygote.jl
+++ src/Zygote.jl
@@ -37,7 +37,7 @@ include("lib/base.jl")
 include("lib/array.jl")
 include("lib/nnlib.jl")
 include("lib/broadcast.jl")
-include("lib/distances.jl")
+@init @require Distances="b4f34e82-e78d-54a5-968a-f98e89d6e8f7" include("lib/distances.jl")
 
 # we need to define this late, so that the genfuncs see lib.jl
 include("compiler/interface2.jl")
