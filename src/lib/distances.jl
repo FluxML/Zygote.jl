b4358deed50caf986a0e71aa70eee7f44ac0f027
Mike Innes <mike.j.innes@gmail.com>
2019-02-08 12:10:10 +0000

    fix distances import

--- src/lib/distances.jl
+++ src/lib/distances.jl
@@ -1,6 +1,6 @@
-using Distances
+using .Distances
 
- @adjoint function sqeuclidean(x::AbstractVector, y::AbstractVector)
+@adjoint function sqeuclidean(x::AbstractVector, y::AbstractVector)
   δ = x .- y
   return sum(abs2, δ), function(Δ::Real)
     x̄ = (2 * Δ) .* δ
@@ -8,13 +8,14 @@ using Distances
   end
 end
 
- @adjoint function colwise(s::SqEuclidean, x::AbstractMatrix, y::AbstractMatrix)
+@adjoint function colwise(s::SqEuclidean, x::AbstractMatrix, y::AbstractMatrix)
   return colwise(s, x, y), function (Δ::AbstractVector)
     x̄ = 2 .* Δ' .* (x .- y)
     return nothing, x̄, -x̄
   end
 end
- @adjoint function pairwise(s::SqEuclidean, x::AbstractMatrix, y::AbstractMatrix)
+
+@adjoint function pairwise(s::SqEuclidean, x::AbstractMatrix, y::AbstractMatrix)
   return pairwise(s, x, y), function(Δ)
     x̄ = 2 .* (x * Diagonal(vec(sum(Δ; dims=2))) .- y * Δ')
     ȳ = 2 .* (y * Diagonal(vec(sum(Δ; dims=1))) .- x * Δ)
@@ -22,7 +23,7 @@ end
   end
 end
 
- @adjoint function pairwise(s::SqEuclidean, X::AbstractMatrix)
+@adjoint function pairwise(s::SqEuclidean, X::AbstractMatrix)
   D = pairwise(s, X)
   return D, function(Δ)
     d1, d2 = Diagonal(vec(sum(Δ; dims=1))), Diagonal(vec(sum(Δ; dims=2)))
