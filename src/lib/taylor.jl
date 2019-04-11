c6b56691b96e665b61cb7eb403da24000805794a
Mike J Innes <mike.j.innes@gmail.com>
2019-04-11 10:10:34 +0100

    fixes for matmul

--- src/lib/taylor.jl
+++ src/lib/taylor.jl
@@ -2,14 +2,23 @@ using TaylorSeries
 import Base: *, +
 
 @adjoint Taylor1(order::Integer) = Taylor1(order), _ -> nothing
+@adjoint Taylor1(T::Type, order::Integer) = Taylor1(T, order), _ -> nothing
 
 struct TaylorAdjoint{T<:Number}
   adjoint::Vector{T}
 end
 
 a::TaylorAdjoint * b::Number = TaylorAdjoint([a*b for a in a.adjoint])
+b::Number * a::TaylorAdjoint = a * b
 
 a::TaylorAdjoint + b::TaylorAdjoint = TaylorAdjoint(a.adjoint .+ b.adjoint)
+
+Base.convert(::Type{TaylorAdjoint{T}}, x::TaylorAdjoint{T}) where T = x
+
+Base.convert(::Type{TaylorAdjoint{T}}, x::TaylorAdjoint) where T =
+  TaylorAdjoint([convert(T, x) for x in x.adjoint])
+
+Base.zero(x::TaylorAdjoint{T}) where T = TaylorAdjoint([zero(T) for x in x.adjoint])
 
 derive(t::Taylor1, i::Integer = 1) = constant_term(TaylorSeries.derivative(t, i))
 derive(x::Number, i::Integer = 1) = i == 0 ? x : zero(x)
@@ -21,12 +30,6 @@ end
 resolve(x) = x
 resolve(t̄::TaylorAdjoint) = sum(derive(t̄.adjoint[i], i-1) for i = 1:length(t̄.adjoint))
 resolve(x::AbstractArray) = resolve.(x)
-
-@adjoint convert(T::Type{<:Taylor1}, x::AbstractSeries) =
-  convert(T, x), ȳ -> (nothing, ȳ,)
-
-@adjoint convert(T::Type{<:Taylor1}, x::Number) =
-  convert(T, x), ȳ -> (nothing, resolve(ȳ))
 
 taylor(x) = x
 
