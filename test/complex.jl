3194d3d972374d88adbad8fdedc7b7e49e3a95e8
GiggleLiu <cacate0129@gmail.com>
2019-04-12 18:01:54 +0800

    fix conj, real

--- test/complex.jl
+++ test/complex.jl
@@ -1,3 +1,5 @@
 using Zygote, Test
 
 @test gradient(x -> real(abs(x)*exp(im*angle(x))), 10+20im)[1] ≈ 1
+@test gradient(x -> imag(real(x)+0.3im), 0.3)[1] ≈ 0
+@test gradient(x -> imag(conj(x)+0.3im), 0.3)[1] ≈ -1im
