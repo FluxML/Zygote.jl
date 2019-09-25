0b494e1c94e9798e9e3592c444d368e8dacd617a
Dhairya Gandhi <dhairya@juliacopmuting.com>
2019-09-25 13:30:42 +0530

    remove pow fn

--- src/compiler/interface.jl
+++ src/compiler/interface.jl
@@ -127,12 +127,3 @@ end
 macro code_adjoint(ex)
   :(Adjoint($(code_irm(ex)), varargs = varargs($(esc(:($InteractiveUtils.@which $ex))), length(($(esc.(ex.args)...),)))))
 end
-
-function pow(x, n)
-  r = 1
-  while n > 0
-    n -= 1
-    r *= x
-  end
-  return r
-end
