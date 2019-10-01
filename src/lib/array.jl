94a45773b4e1693f4e5fd7482b20e31f2d185b91
Seth Axen <seth.axen@gmail.com>
2019-10-01 01:12:48 -0700

    Add custom adjoint for cos

--- src/lib/array.jl
+++ src/lib/array.jl
@@ -461,7 +461,7 @@ end
   (Ā, )
 end
 
-for func in (:exp, :sin, :tan, :cosh, :sinh, :tanh, :atan, :asinh, :atanh)
+for func in (:exp, :cos, :sin, :tan, :cosh, :sinh, :tanh, :atan, :asinh, :atanh)
   @eval begin
     @adjoint function ($func)(A::LinearAlgebra.RealHermSymComplexHerm)
       λ,U = eigen(A)
