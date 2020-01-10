2098e739a7d69a91454690364d6ec0c143a03271
Mike Innes <mike.j.innes@gmail.com>
2020-01-10 17:54:31 +0000

    1.4 updates fixes #393

--- src/lib/lib.jl
+++ src/lib/lib.jl
@@ -159,6 +159,16 @@ unapply(t, xs) = _unapply(t, xs)[1]
   end
 end
 
+@adjoint! function Core._apply_iterate(::typeof(iterate), f, args...)
+  y, back = Core._apply(_pullback, (__context__, f), args...)
+  st = map(_empty, args)
+  y, function (Δ)
+    Δ = back(Δ)
+    Δ === nothing ? nothing :
+      (nothing, first(Δ), unapply(st, Base.tail(Δ))...)
+  end
+end
+
 # Structs
 
 deref!(x) = x
