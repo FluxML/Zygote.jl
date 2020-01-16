8f2c6558f9a78fec2eadae69eb0b052e0a2993ff
cossio <j.cossio.diaz@gmail.com>
2020-01-16 18:13:52 +0100

    nograd

--- src/lib/grad.jl
+++ src/lib/grad.jl
@@ -1,5 +1,11 @@
 using MacroTools: @q
 
+"""
+    @nograd(f...)
+
+The output of the argument functions will be considered as constant
+and will not contribute gradients.
+"""
 macro nograd(ex)
   isexpr(ex, :tuple) || (ex = Expr(:tuple, ex))
   blk = @q begin end
@@ -9,6 +15,23 @@ macro nograd(ex)
   end
   return blk
 end
+
+"""
+    nograd(x)
+
+Treats `x` as a constant. For example:
+
+```julia-repl
+julia> f(x) = sum(nograd(x) + x); gradient(f, randn(5))
+([1.0, 1.0, 1.0, 1.0, 1.0],)
+```
+
+In the example above, the gradient is an array of ones even though
+`f(x)` computes `sum(2x)`, because `nograd(x)` uses the value of `x`
+as if it were a constant.
+"""
+nograd(x) = x
+@nograd nograd
 
 macro which(ex)
   @capture(ex, f_(args__)) || error("Zygote.@which f(args...)")
