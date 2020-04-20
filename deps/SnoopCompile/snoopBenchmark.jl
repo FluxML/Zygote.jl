0f28c81b4cbb1d9d46ea6bc43e2cf690012139bd
Ian <i.r.butterworth@gmail.com>
2020-04-20 16:21:03 -0400

    add compiler test to benchmark

--- deps/SnoopCompile/snoopBenchmark.jl
+++ deps/SnoopCompile/snoopBenchmark.jl
@@ -1,4 +1,10 @@
 using SnoopCompile
 println("loading infer benchmark")
 
-@snoopi_bench "Zygote" using Zygote
+@snoopi_bench "Zygote loading" begin
+    using Zygote
+end
+
+@snoopi_bench "Zygote compiler tests" begin
+    include(joinpath(@__DIR__,"..","..","test","compiler.jl"))
+end
