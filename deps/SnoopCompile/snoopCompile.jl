9032b4d8cac5f7087de651c375ae2be8ebce270a
Ian <i.r.butterworth@gmail.com>
2020-04-21 17:07:16 -0400

    tweak precompile script [skip ci]

--- deps/SnoopCompile/snoopCompile.jl
+++ deps/SnoopCompile/snoopCompile.jl
@@ -2,5 +2,23 @@ using SnoopCompile
 
 @snoopi_bot "Zygote" begin
   using Zygote
-  include(joinpath(pkgdir(Zygote),"src","precompile.jl"))
+  using InteractiveUtils
+
+  function pow(x, n)
+    r = 1
+    while n > 0
+      n -= 1
+      r *= x
+    end
+    return r
+  end
+
+  Zygote.@code_adjoint pow(2, 3)
+
+  Zygote.@code_adjoint ((x, y) -> sum(x.*y))([1,2,3],[4,5,6])
+
+  Zygote.gradient(pow, 2, 3)
+  Zygote.gradient((x, y) -> sum(x.*y), [1, 2, 3], [4, 5, 6])
+
+  Zygote.gradient(x -> sum(Float32.(x)), [1.0])
 end
