f472b5c756199572ddcd853a70eb8be502d635c2
aminya <aminyahyaabadi74@gmail.com>
2020-04-23 01:03:59 -0500

    using absolute path

--- deps/SnoopCompile/snoopi_bench.jl
+++ deps/SnoopCompile/snoopi_bench.jl
@@ -7,5 +7,7 @@ end
 
 println("Benchmarking the inference time of `using Zygote` & basic function test")
 @snoopi_bench BotConfig("Zygote") begin
-    include("example_script.jl")
+    using Zygote
+    zygote_rootpath = dirname(dirname(pathof(Zygote)))
+    include("$zygote_rootpath/deps/SnoopCompile/example_script.jl")
 end
--- deps/SnoopCompile/snoopi_bot.jl
+++ deps/SnoopCompile/snoopi_bot.jl
@@ -2,5 +2,7 @@ using SnoopCompile
 
 @snoopi_bot BotConfig("Zygote") begin
   # we should hide them in a file, so Julia doesn't exapnd the macros before executing `using Zygote`
-  include("example_script.jl")
+  using Zygote
+  zygote_rootpath = dirname(dirname(pathof(Zygote)))
+  include("$zygote_rootpath/deps/SnoopCompile/example_script.jl")
 end
