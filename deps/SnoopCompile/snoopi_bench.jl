dbbd4a71de34f8011fefd9175e224c4d986ae10e
aminya <aminyahyaabadi74@gmail.com>
2020-04-24 14:47:23 -0500

    using julia 1.3 and 1.2

--- deps/SnoopCompile/snoopi_bench.jl
+++ deps/SnoopCompile/snoopi_bench.jl
@@ -2,13 +2,13 @@ using SnoopCompile
 
 println("Benchmarking the inference time of `using Zygote`")
 snoopi_bench(
-  BotConfig("Zygote"; os = ["linux", "windows", "macos"], version = [v"1.4.1", v"1.0.5"]),
+  BotConfig("Zygote"; os = ["linux", "windows", "macos"], version = [v"1.4.1", v"1.3.1", v"1.2.0"]),
   :(using Zygote)
 )
 
 
 println("Benchmarking the inference time of `using Zygote` & basic function test")
 snoopi_bench(
-  BotConfig("Zygote"; os = ["linux", "windows", "macos"], version = [v"1.4.1", v"1.0.5"]),
+  BotConfig("Zygote"; os = ["linux", "windows", "macos"], version = [v"1.4.1", v"1.3.1", v"1.2.0"]),
   "$(@__DIR__)/example_script.jl",
 )
--- deps/SnoopCompile/snoopi_bot.jl
+++ deps/SnoopCompile/snoopi_bot.jl
@@ -1,6 +1,6 @@
 using SnoopCompile
 
 snoopi_bot(
-  BotConfig("Zygote"; os = ["linux", "windows", "macos"], version = [v"1.4.1", v"1.0.5"]),
+  BotConfig("Zygote"; os = ["linux", "windows", "macos"], version = [v"1.4.1", v"1.3.1", v"1.2.0"]),
   "$(@__DIR__)/example_script.jl",
 )
