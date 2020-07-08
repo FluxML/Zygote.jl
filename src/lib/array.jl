fc53975e28f39aee25dd52d9c81f755a941ac386
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-07-08 09:47:35 -0400

    support pmap

--- src/lib/array.jl
+++ src/lib/array.jl
@@ -170,23 +170,25 @@ function unzip(tuples)
   _unzip(tuples, Val(N))
 end
 
-function ∇map(cx, f, args...)
-  ys_and_backs = map((args...) -> _pullback(cx, f, args...), args...)
-  if isempty(ys_and_backs)
-    ys_and_backs, _ -> nothing
-  else
-    ys, backs = unzip(ys_and_backs)
-    ys, function (Δ)
-      Δf_and_args_zipped = map((f, δ) -> f(δ), backs, Δ)
-      Δf_and_args = unzip(Δf_and_args_zipped)
-      Δf = reduce(accum, Δf_and_args[1])
-      (Δf, Δf_and_args[2:end]...)
+for mapfunc,∇mapfunc in [(:map,:∇map),(:pmap,:∇pmap)]
+  @eval function $∇mapfunc(cx, f, args...)
+    ys_and_backs = $mapfunc((args...) -> _pullback(cx, f, args...), args...)
+    if isempty(ys_and_backs)
+      ys_and_backs, _ -> nothing
+    else
+      ys, backs = unzip(ys_and_backs)
+      ys, function (Δ)
+        Δf_and_args_zipped = $mapfunc((f, δ) -> f(δ), backs, Δ)
+        Δf_and_args = unzip(Δf_and_args_zipped)
+        Δf = reduce(accum, Δf_and_args[1])
+        (Δf, Δf_and_args[2:end]...)
+      end
     end
   end
-end
 
-@adjoint function map(f, args::Union{AbstractArray,Tuple}...)
-  ∇map(__context__, f, args...)
+  @eval @adjoint function $mapfunc(f, args::Union{AbstractArray,Tuple}...)
+    $∇mapfunc(__context__, f, args...)
+  end
 end
 
 function _pullback(cx::AContext, ::typeof(collect), g::Base.Generator)
