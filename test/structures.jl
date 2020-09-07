0a283f56eeaba4c2a08beb741e6da317309d6f0b
Dhairya Gandhi <dhairya@juliacopmuting.com>
2020-09-07 10:14:22 +0530

    typo

--- test/structures.jl
+++ test/structures.jl
@@ -30,7 +30,7 @@ end
 tasks4(x) = fetch(@async x^2)
 @test gradient(tasks4, 5) == (10,)
 
-tasks5(x) = fetch(schedule(Task(() -> x^2))
+tasks5(x) = fetch(schedule(Task(() -> x^2)))
 @test gradient(tasks5, 5) == (10,)
 
 VERSION > v"1.3-" && include("threads.jl")
