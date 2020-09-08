1476352acfa1903dc6189f1ba890f7a68414da03
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2020-09-08 18:14:08 +0100

    test suite

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,51 +2,52 @@ using Zygote, Test
 using Zygote: gradient
 using CUDA: has_cuda
 
-@testset "Interface" begin
-  include("interface.jl")
-end
-
-
-@testset "Tools" begin
-  include("tools.jl")
-end
-
-@testset "lib/number" begin
-  include("lib/number.jl")
-end
-
-@testset "Features" begin
-  include("features.jl")
-end
-
-@testset "Forward" begin
-  include("forward/forward.jl")
-end
-
-@testset "Data Structures" begin
-  include("structures.jl")
-end
-
-@testset "ChainRules" begin
-  include("chainrules.jl")
-end
-
-@testset "Gradients" begin
-  include("gradcheck.jl")
-end
-
-@testset "Complex" begin
-  include("complex.jl")
-end
-
-@testset "Compiler" begin
-  include("compiler.jl")
-end
-
-if has_cuda()
-  @testset "CUDA tests" begin
-    include("cuda.jl")
+@testset "Zygote" begin
+  @testset "Interface" begin
+    include("interface.jl")
   end
-else
-  @warn "CUDA not found - Skipping CUDA Tests"
+
+  @testset "Tools" begin
+    include("tools.jl")
+  end
+
+  @testset "lib/number" begin
+    include("lib/number.jl")
+  end
+
+  @testset "Features" begin
+    include("features.jl")
+  end
+
+  #@testset "Forward" begin
+  #  include("forward/forward.jl")
+  #end
+
+  @testset "Data Structures" begin
+    include("structures.jl")
+  end
+
+  @testset "ChainRules" begin
+    include("chainrules.jl")
+  end
+
+  @testset "Gradients" begin
+    include("gradcheck.jl")
+  end
 end
+
+#@testset "Complex" begin
+#  include("complex.jl")
+#end
+#
+#@testset "Compiler" begin
+#  include("compiler.jl")
+#end
+#
+#if has_cuda()
+#  @testset "CUDA tests" begin
+#    include("cuda.jl")
+#  end
+#else
+#  @warn "CUDA not found - Skipping CUDA Tests"
+#end
