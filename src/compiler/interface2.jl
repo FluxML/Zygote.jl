53baaf27bc55891184639e077fae1b327e5874e5
Miha Zgubic <mzgubic@users.noreply.github.com>
2020-09-16 15:49:47 +0100

    Update src/compiler/interface2.jl
    Co-authored-by: Lyndon White <oxinabox@ucc.asn.au>

--- src/compiler/interface2.jl
+++ src/compiler/interface2.jl
@@ -41,7 +41,7 @@ end
     rethrow(CompileError(T,e))
   end
   if g == nothing
-    Δ <: AbstractZero && return :(DoesNotExist())
+    Δ <: AbstractZero && return :(Δ)
     if Δ == Nothing
         Core.println("hit 'nothing' in (j::Pullback{T})(Δ)")
         return :(DoesNotExist())
