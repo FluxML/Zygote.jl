5c04890c24e5f431c674a2d782adcb1469676bde
Dhairya Gandhi <dhairya@juliacopmuting.com>
2020-12-12 20:43:34 +0530

    add isderiving

--- src/lib/utils.jl
+++ src/lib/utils.jl
@@ -111,3 +111,26 @@ a real.
      1  0
 """
 hessian(f, x::AbstractArray) = forward_jacobian(x -> gradient(f, x)[1], x)[2]
+
+"""
+   isderiving()
+
+Check whether the current function call is happening while taking the derivative.
+
+
+    julia> function f(x)
+             @show isderiving()
+           end
+    
+    f (generic function with 1 method)
+    
+    julia> f(3)
+    isderiving() = false
+    false
+    
+    julia> gradient(f, 4)
+    isderiving() = true
+    (nothing,)
+"""
+isderiving() = false
+@adjoint isderiving() = true, _ -> nothing
