83f920e2268fae6a59c0f571b0353823d157c994
Jan Weidner <jw3126@gmail.com>
2021-01-19 14:40:34 +0100

    Update src/lib/grad.jl
    Co-authored-by: willtebbutt <wt0881@my.bristol.ac.uk>

--- src/lib/grad.jl
+++ src/lib/grad.jl
@@ -23,7 +23,7 @@ Use gradient checkpointing on the call `f(xs...)`. This means that
 `checkpointed(f, xs...) === f(xs...)`, but when computing the derivative
 intermediate results from the forward pass of `f` will not be stored. Instead the forward
 pass will be repeated, when computing the derivative.
-This this saves memory at the cost of inreasing exectution time.
+This this saves memory at the cost of increasing exectution time.
 """
 checkpointed(f, xs...) = f(xs...)
 
