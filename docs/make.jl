ddfb5ad12240cc0048748b59460dd81fa1184c54
Kyle Daruwalla <daruwalla@wisc.edu>
2021-03-29 15:16:25 -0500

    Activate docs directory

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,6 @@
 using Pkg;
 Pkg.activate(joinpath(@__DIR__, "..")); Pkg.instantiate()
-Pkg.activate(); Pkg.instantiate()
+Pkg.activate(@__DIR__); Pkg.instantiate()
 
 pushfirst!(LOAD_PATH, joinpath(@__DIR__, ".."))
 
