d7cd2ec8be5784bae32ef6276acb5fe6628d2397
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2021-07-12 21:02:02 -0400

    tweak

--- src/compiler/interface.jl
+++ src/compiler/interface.jl
@@ -200,7 +200,6 @@ function Base.delete!(ps::Params, x)
 end
 
 Base.Broadcast.broadcasted(f, ps::Params) = broadcasted(f, ps.order)
-Base.Broadcast.broadcastable(ps::Params) = ps.order
 
 @adjoint function Broadcast.broadcasted(f::Function, ps::Params)
   f.(ps), _ -> throw(ArgumentError("Zygote.Params does not support broadcasting within gradients, try iteration `for p in ps`"))
