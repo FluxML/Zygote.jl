3df54fbc7d6d2d4fc68a1c190ef047fcc933d832
Carlo Lucibello <carlo.lucibello@gmail.com>
2021-07-19 11:00:21 +0200

    use splatting

--- src/tools/idset.jl
+++ src/tools/idset.jl
@@ -7,13 +7,7 @@ IdSet(xs) = IdSet{eltype(xs)}(xs)
 
 IdSet() = IdSet{Any}()
 
-function IdSet{T}(xs) where T
-  s = IdSet{T}()
-  for x in xs
-    push!(s, x)
-  end
-  return s
-end
+IdSet{T}(xs) = isempty(xs) ? IdSet{T}() : push!(IdSet{T}(), xs...)
 
 Base.push!(s::IdSet{T}, x::T) where T = (s.dict[x] = nothing; s)
 Base.delete!(s::IdSet{T}, x::T) where T = (delete!(s.dict, x); s)
