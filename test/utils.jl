26286fc64c48149bb7a6c0b6e1108147ad1e3a5b
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2021-07-23 13:31:51 +0200

    mark ForwardDiff test broken

--- test/utils.jl
+++ test/utils.jl
@@ -24,10 +24,15 @@ end
 
   xs, y = randn(2,3), rand()
   f34(xs, y) = xs[1] * (sum(xs .^ (1:3)') + y^4)  # non-diagonal Hessian, two arguments
-  dx, dy = diaghessian(f34, xs, y)
-  @test size(dx) == size(xs)
-  @test vec(dx) ≈ diag(hessian(x -> f34(x,y), xs))
-  @test dy ≈ hessian(y -> f34(xs,y), y)
+
+  function broken()
+    dx, dy = diaghessian(f34, xs, y) # This fails becase ProjectTo can't project a Dual onto a Float
+    c1 = size(dx) == size(xs)
+    c2 = vec(dx) ≈ diag(hessian(x -> f34(x,y), xs))
+    c3 = dy ≈ hessian(y -> f34(xs,y), y)
+    return all([c1, c2, c3])
+  end
+  @test_broken broken()
 
   zs = randn(7,13)  # test chunk mode
   @test length(zs) > ForwardDiff.DEFAULT_CHUNK_THRESHOLD
