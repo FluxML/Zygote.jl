0bb31c277af74bcc1cfe5101c6b363981d3fb91e
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2021-08-01 10:42:33 -0400

    better dims handling in unbroadcast

--- src/lib/broadcast.jl
+++ src/lib/broadcast.jl
@@ -45,10 +45,16 @@ function Base.reducedim_init(::typeof(identity), ::typeof(accum), A::AbstractArr
   Base.reducedim_initarray(A, region, nothing, Union{Nothing,eltype(A)})
 end
 
-unbroadcast(x::AbstractArray, x̄) =
-  length(x) == length(x̄) ? _project(x, x̄) :
-    _project(x, accum_sum(x̄, dims = ntuple(i -> size(x, i) == 1 ? i : ndims(x̄)+1, Val(ndims(x̄)))))
-
+function unbroadcast(x::AbstractArray, x̄)
+  N = ndims(x̄)
+  if length(x) == length(x̄)
+    _project(x, x̄)  # ProjectTo handles reshape, offsets, structured matrices
+  else
+    tup = filter(d -> !(d isa Nothing), ntuple(i -> size(x, i) == 1 ? i : nothing, N))
+    dims = tup isa Tuple{Int} ? only(tup) : tup  # avoid sum(xbar, dims=(1,)) as e.g. sum(SA[1 2; 3 4], dims=(1,)) fails
+    _project(x, accum_sum(x̄; dims = dims))
+  end
+end
 unbroadcast(x::Number, x̄) = accum_sum(x̄)
 unbroadcast(x::Tuple{<:Any}, x̄) = (accum_sum(x̄),)
 unbroadcast(x::Base.RefValue, x̄) = (x=accum_sum(x̄),)
