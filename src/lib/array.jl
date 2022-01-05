448ba91f4668bb1163166ab2c5782a6c7cdbdb2e
Joe Greener <jgreener@hotmail.co.uk>
2022-01-05 14:17:50 +0000

    Check for unique indices

--- src/lib/array.jl
+++ src/lib/array.jl
@@ -44,7 +44,13 @@ import NNlib
 
 ∇getindex(x::CuArray, inds::Tuple{AbstractArray{<:Integer}}) = dy -> begin
   dx = _zero(x, eltype(dy))
-  NNlib.scatter!(+, dx, dy, inds[1])
+  inds1_cpu = Array(inds[1])
+  if allunique(inds1_cpu)
+    dxv = view(dx, inds[1])
+    dxv .= accum.(dxv, _droplike(dy, dxv))
+  else
+    NNlib.scatter!(+, dx, dy, inds1_cpu)
+  end
   return (_project(x, dx), map(_->nothing, inds)...)
 end
 
