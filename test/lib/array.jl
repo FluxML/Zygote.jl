1dc41e5a7bd2a5b41cc459874f381cf6e15e868e
Sam Buercklin <sam@metalenz.com>
2022-02-15 12:37:57 -0500

    added gradient test, remove lib/array test

--- test/lib/array.jl
+++ test/lib/array.jl
@@ -6,7 +6,3 @@ using Zygote: ZygoteRuleConfig
 
 test_rrule(ZygoteRuleConfig(), x->sum(sin, Diagonal(x)), ones(2); rrule_f=rrule_via_ad, check_inferred=false)
 test_rrule(ZygoteRuleConfig(), x->sum(sin, Diagonal(x)), rand(3); rrule_f=rrule_via_ad, check_inferred=false)
-
-# issue 44179
-
-@test Zygote._restore([nothing, 1, nothing], Val(3)) == (nothing, 1, nothing)
