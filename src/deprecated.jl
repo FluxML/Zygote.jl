99149d4c345a6ea4ddf5a994e5c897381031bcc0
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2022-06-17 17:48:58 +0200

    deprecate ignore

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -13,3 +13,39 @@ function dropgrad end
 @adjoint dropgrad(x) = dropgrad(x), _ -> nothing
 
 Base.@deprecate dropgrad(x) ChainRulesCore.ignore_derivatives(x)
+
+
+"""
+    ignore() do
+      ...
+    end
+
+Tell Zygote to ignore a block of code. Everything inside the `do` block will run
+on the forward pass as normal, but Zygote won't try to differentiate it at all.
+This can be useful for e.g. code that does logging of the forward pass.
+
+Obviously, you run the risk of incorrect gradients if you use this incorrectly.
+"""
+function ignore end
+
+@adjoint ignore(f) = ignore(f), _ -> nothing
+
+Base.@deprecate ignore(f) ChainRulesCore.ignore_derivatives(f)
+
+"""
+    @ignore (...)
+
+Tell Zygote to ignore an expression. Equivalent to `ignore() do (...) end`.
+Example:
+
+```julia-repl
+julia> f(x) = (y = Zygote.@ignore x; x * y);
+julia> f'(1)
+1
+```
+"""
+macro ignore(ex)
+    return :(Zygote.ignore() do
+        $(esc(ex))
+    end)
+end
