0e2e4b8430ad4a97b5bbe1e09cf31660d8440734
Shambles <yicheng.wu@ucalgary.ca>
2022-07-19 00:47:58 -0600

    Update array.jl

--- src/lib/array.jl
+++ src/lib/array.jl
@@ -75,7 +75,7 @@ the elements of arrays in place (e.g. setting values with x .= ...)
 Possible fixes:
 - avoid mutating operations (preferred)
 - or read the documentation and solutions for this error
-  https://fluxml.ai/Zygote.jl/dev/limitations.html#Array-mutation-1
+  https://fluxml.ai/Zygote.jl/dev/limitations/#Array-mutation-1
 """)
 
 @adjoint! setindex!(xs::AbstractArray, x...) = setindex!(xs, x...),
