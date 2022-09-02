ea21486bcd50b2e161d3aff21652f78f743711e6
maartenvd <maartenvd1994@gmail.com>
2022-09-02 10:27:50 +0200

    simple test

--- test/lib/base.jl
+++ test/lib/base.jl
@@ -1,3 +1,5 @@
+using LinearAlgebra;
+
 @testset "base.jl" begin
     @testset "Dict getindex with implicit params" begin
         d = Dict{String, Vector{Float64}}("key"=>ones(4))
@@ -9,5 +11,12 @@
         result2 = gradient(fn2, Params([x]))[x]
 
         @test result1 == result2
+    end
+
+    @testset "Type stability under broadcast" begin
+        g_simple = gradient(a->sum(broadcast(x->x+3,a)),Diagonal([1,2,3]));
+        g_hard = gradient((a,b)->sum(broadcast(x->x*b,a)),Diagonal([1,2,3]),4);
+        @test first(g_simple) isa Diagonal
+        @test first(g_hard) isa Diagonal
     end
 end
