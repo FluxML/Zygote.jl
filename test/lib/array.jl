62ca9c1a552811c0f073f2aaab7fc14d26872d4d
Carlo Lucibello <carlo.lucibello@gmail.com>
2022-11-23 14:38:41 +0100

    add tests

--- test/lib/array.jl
+++ test/lib/array.jl
@@ -18,3 +18,19 @@ test_rrule(ZygoteRuleConfig(), x->sum(sin, Diagonal(x)), rand(3); rrule_f=rrule_
     @test gradient(x -> sum([y[2] * y[3] for y in Iterators.product(x, x, x, x)]), [1,2,3,4])[1] ≈ [320, 320, 320, 320]
     @test gradient(x -> sum(y[2] * y[3] for y in Iterators.product(x, x, x, x)), [1,2,3,4])[1] ≈ [320, 320, 320, 320]
 end
+
+@testset "collect" begin
+    @testset "Dict" begin
+        d = Dict(1 => 5, 2 => 6)
+        k = 2
+        i = findfirst(p -> p[1] == k, collect(d))
+
+        @test gradient(d -> collect(d)[i][2], d)[1][k] == 1
+        @test gradient(d -> sum(v^2 for (_,v) in collect(d)), d) == (Dict(1 => 10, 2 => 12),)
+    end
+
+    @testset "NamedTuple" begin
+        t = (a=1, b=2)
+        @test gradient(d -> sum(x^2 for x in collect(d)), t) == ((a = 2, b = 4),)
+    end
+end
