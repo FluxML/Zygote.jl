81680a7b8da780aad2235e8389a8fbd4517bd8c2
Carlo Lucibello <carlo.lucibello@unibocconi.it>
2022-11-25 05:55:35 +0100

    tests for dictionaries (#1330)
    * tests for issue 717
    
    * test for issue 760
    
    * fix tests
    
    * add .vscode to gitignore
    
    * cleanup

--- test/features.jl
+++ test/features.jl
@@ -817,3 +817,21 @@ end
   @test gradient(xs -> sum(map(x->x.im^2, xs)), [1+2im,3])[1] == [4im, 0]
   @test gradient(xs -> mapreduce(x->x.im^2, +, xs), [1+2im,3])[1] == [4im, 0]
 end
+
+@testset "Dict" begin
+  # issue #717
+  @test gradient(x -> (() -> x[:y])(), Dict(:y => 0.4)) == (Dict(:y => 1.0),)
+
+  ntd = (; data = Dict("x" => rand(2)))
+  @test gradient(x -> sum(x.data["x"]), ntd)[1] == (; data = Dict("x" => ones(2)))
+
+  # issue #760
+  function f760(x)
+    d = Dict()
+    for i in 1:4
+        push!(d, i=>i^x)
+    end
+    sum(values(d))
+  end
+  @test gradient(f760, 3)[1] ≈ 123.93054835019153
+end
