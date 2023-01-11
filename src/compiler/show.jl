9df7cd8b2d6118e4c9dc54220efe4cb102baf82e
Takuya Iwanaga <takuyai@gmail.com>
2023-01-11 18:18:52 +1100

    auto-format

--- src/compiler/show.jl
+++ src/compiler/show.jl
@@ -1,11 +1,11 @@
-funcname(::Type{Type{T}}) where T = string(T)
+funcname(::Type{Type{T}}) where {T} = string(T)
 
 function funcname(T)
-  if isdefined(T, :instance)
-    string(T.instance)
-  else
-    "λ"
-  end
+    if isdefined(T, :instance)
+        string(T.instance)
+    else
+        "λ"
+    end
 end
 
 Base.show(io::IO, j::Pullback{S}) where S = print(io, "∂($(funcname(S.parameters[1])))")
