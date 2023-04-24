a0f2327eb4337f13de1355ee69d432259e52661d
Tim Holy <tim.holy@gmail.com>
2023-04-24 16:31:46 -0500

    Migrate from SnoopPrecompile to PrecompileTools

--- src/Zygote.jl
+++ src/Zygote.jl
@@ -77,11 +77,11 @@ macro profile(ex)
   end
 end
 
-using SnoopPrecompile
+using PrecompileTools
 # This caused freezes on early 1.8 patch versions,
 # see https://github.com/SciML/DiffEqFlux.jl/issues/783
 @static if VERSION < v"1.8" || VERSION >= v"1.8.5"
-  @precompile_all_calls precompile()
+  @compile_workload precompile()
 end
 
 end # module
