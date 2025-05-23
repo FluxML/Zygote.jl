d8963906c8f7d0b958cd029a9b868236b84ed735
DhairyaLGandhi <dhairya@juliacomputing.com>
2025-05-24 04:08:13 +0530

    fix: accumulate mutable structs properly

--- src/lib/lib.jl
+++ src/lib/lib.jl
@@ -15,6 +15,7 @@ accum(x, y, zs...) = accum(accum(x, y), zs...)
 
 accum(x::Tuple, ys::Tuple...) = map(accum, x, ys...)
 accum(x::AbstractArray, ys::AbstractArray...) = Base.broadcast_preserving_zero_d(accum, x, ys...)
+accum(::Tuple{}, ::NamedTuple{}) = ()
 
 @generated function accum(x::NamedTuple, y::NamedTuple)
   # assumes that y has no keys apart from those also in x
@@ -230,7 +231,7 @@ end
     else
       dx = grad_mut(__context__, x)
       dx[] = (; dx[]..., pair(Val(f), accum(getfield(dx[], f), Δ))...)
-      return (dx,nothing)
+      return (dx[],nothing)
     end
   end
   unwrap(val), back
