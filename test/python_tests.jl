d48e644b1de1c8252c329a9e6e1d25d1050ad760
Carlo Lucibello <carlo.lucibello@gmail.com>
2025-10-26 19:41:10 +0100

    skip python on v1.10

--- test/python_tests.jl
+++ test/python_tests.jl
@@ -1,12 +1,11 @@
-@testitem "pythoncall" begin
+@testitem "pythoncall" skip=(VERSION < v"1.11") begin
 
-using PythonCall: pyimport, pyconvert
+  using PythonCall: pyimport, pyconvert
 
-@testset "PythonCall custom @adjoint" begin
-  math = pyimport("math")
-  pysin(x) = math.sin(x)
-  Zygote.@adjoint pysin(x) = pyconvert(Float64, math.sin(x)), δ -> (pyconvert(Float64, δ * math.cos(x)),)
-  @test Zygote.gradient(pysin, 1.5) == Zygote.gradient(sin, 1.5)
-end
-
+  @testset "PythonCall custom @adjoint" begin
+    math = pyimport("math")
+    pysin(x) = math.sin(x)
+    Zygote.@adjoint pysin(x) = pyconvert(Float64, math.sin(x)), δ -> (pyconvert(Float64, δ * math.cos(x)),)
+    @test Zygote.gradient(pysin, 1.5) == Zygote.gradient(sin, 1.5)
+  end
 end
